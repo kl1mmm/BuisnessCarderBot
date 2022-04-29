deba0466fdd879d5634d47bfa0f81a702e4c5ba7
kl1mmm <klimovilya07@mail.ru>
2022-04-29 09:22:09 +0900

    Добавлена функция стартового сообщения бота.

--- Bot.py
+++ Bot.py
@@ -2,5 +2,16 @@
 
 import telebot
 from Setting import ApiKeyTelegramBot
+from Markup import markupStart
+from String import MessageStart
 
 bot = telebot.TeleBot(ApiKeyTelegramBot)
+
+@bot.message_handler(commands=['start'])
+def start(msg):
+    bot.send_message(msg.chat.id, MessageStart, reply_markup=markupStart)
+
+def main():
+    bot.polling(none_stop=True)
+if __name__ == '__main__':
+    main()

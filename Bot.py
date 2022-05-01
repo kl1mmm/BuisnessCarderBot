c6808f7b68dfa87c640a1c7ee518fc261ac33f33
kl1mmm <klimovilya07@mail.ru>
2022-05-01 15:44:45 +0900

    UPD: ответы и MessageText. NFix: экранные кнопки.

--- Bot.py
+++ Bot.py
@@ -3,15 +3,39 @@
 import telebot
 from Setting import ApiKeyTelegramBot
 from Markup import markupStart
+from Markup import markupScreen
 from String import MessageStart
+from String import MessageStartActive
+from String import MessageScreen1
+from String import MessageError
+from String import MessageTest
 
 bot = telebot.TeleBot(ApiKeyTelegramBot)
 
+#-Отправка стартового сообщения-------------------------------------------------------------------------------------------------------
 @bot.message_handler(commands=['start'])
 def start(msg):
     bot.send_message(msg.chat.id, MessageStart, reply_markup=markupStart)
 
-def main():
-    bot.polling(none_stop=True)
-if __name__ == '__main__':
-    main()
+
+#-Обработка текстовых комманд---------------------------------------------------------------------------------------------------------
+@bot.message_handler(content_types=['text'])
+def textByUser(msg):
+    if msg.chat.type == 'private':
+        if msg.text == 'Экранные кнопки':
+            bot.send_message(msg.chat.id, MessageScreen1, reply_markup=markupScreen)
+        elif msg.text == 'Начать создание визитки':
+            bot.send_message(bot.send_message(msg.chat.id, MessageStartActive, reply_markup=markupScreen))
+        else:
+            bot.send_message(msg.chat.id, MessageError, reply_markup=markupStart)
+
+
+#-Обработка экранных кнопок-----------------------------------------------------------------------------------------------------------
+@bot.callback_query_handler(func=lambda c: c.data)
+def answer_callback(callback):
+    if callback.data == '1':
+        MessageTest = 'Пикнул кнопку'
+    elif callback.data == '2':
+        MessageTest = 'Пикнул кнопку'
+
+bot.polling(none_stop = True)
--- Markup.py
+++ Markup.py
@@ -1,6 +1,16 @@
 #Файл в котором будет находится клавиатура и кнопки используемы ботом
 from telebot import types
 
-StartKey = types.KeyboardButton('Нажми, чтобы начать работу')
+#-Кнопки-----------------------------------------------------------------------------------------------------------------------
+StartKey = types.KeyboardButton(text='Начать создание визитки')
+
+KeyScreen = types.KeyboardButton('Экранные кнопки')
+
+Key1 = types.InlineKeyboardButton(text='Кнопка 1', callback_data='1')
+Key2 = types.InlineKeyboardButton(text='Кнопка 2', callback_data='2')
+#-Меню старта------------------------------------------------------------------------------------------------------------------
 markupStart = types.ReplyKeyboardMarkup(resize_keyboard=True)
 markupStart.add(StartKey)
+#-Меню на экране---------------------------------------------------------------------------------------------------------------
+markupScreen = types.ReplyKeyboardMarkup()
+markupScreen.add(Key1, Key2)

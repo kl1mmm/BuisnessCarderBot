a8953b6443ccab409b97ee554fdbe41ebc28a4f8
kl1mmm <klimovilya07@mail.ru>
2022-05-15 12:31:56 +0900

    ебал я в рот

--- Bot.py
+++ Bot.py
@@ -5,9 +5,12 @@ from Setting import ApiKeyTelegramBot
 from Markup import markupStart
 from Markup import markupScreen
 from Markup import markupMessage
+from Markup import markupAddContacts
 from String import *
 
 bot = telebot.TeleBot(ApiKeyTelegramBot)
+
+DataClient = []
 
 #-Отправка стартового сообщения-------------------------------------------------------------------------------------------------------
 @bot.message_handler(commands=['start'])
@@ -33,20 +36,47 @@ def textByUser(msg):
         else:
             bot.send_message(msg.chat.id, MessageError, reply_markup=markupStart)
 
-@bot.callback_query_handler(func=lambda c: c.data)
-def card_handling(callback):
-    PhotoAdd, DiscripAdd, ContactsAdd = False, False, False
-    if callback.data == 'photo':
-        PhotoAdd = True
-        bot.answer_callback_query(callback.id, text="Ваше фото успешно загружено!")
-    if callback.data == 'discription':
-        DiscripAdd = True
-        bot.answer_callback_query(callback.id, text="Отлично. Теперь описание реализовано.")
-    if callback.data == 'contacts':
-        ContactsAdd = True
-        bot.answer_callback_query(callback.id, text="Контакты были внесены в визитку.")
+@bot.message_handler(content_types=['text'])
+def after_text_Discrip(msg):
+    DataClient[0] = msg.Text
+def after_text_Phone(msg):
+    DataClient[1] = msg.Text
+def after_text_Email(msg):
+    DataClient[2] = msg.Text
+def after_text_Address(msg):
+    DataClient[3] = msg.Text
+
 
 #-Обработка экранных кнопок-----------------------------------------------------------------------------------------------------------
+@bot.callback_query_handler(func=lambda c: c.data)
+def card_handling(callback):
+    DiscripAdd, ContactsAdd = False, False
+    ###if callback.data == 'photo':
+    ###    PhotoAdd = True
+    ###    bot.answer_callback_query(callback.id, text="Ваше фото успешно загружено!")
+    if callback.data == 'discription':
+        bot.send_message(chat_id=callback.message.chat.id, text=MessageAskDiscrip)
+        bot.send_message(chat_id=callback.message.chat.id, text=MessageDiscripExample)
+
+
+        bot.answer_callback_query(callback.id, text="Отлично. Теперь описание реализовано.")
+        DiscripAdd = True
+    if callback.data == 'contacts':
+        Phone, Email, Address = False, False, False
+        bot.send_message(chat_id=callback.message.chat.id, text=MessageAskContacts, reply_markup=markupAddContacts)
+        if callback.data == 'phone':
+            msg = bot.send_message(chat_id=callback.message.chat.id, text='Отправь нужный номер телефона')
+            bot.register_next_step_handler(msg, after_text_Phone)
+            bot.answer_callback_query(callback.id, text="Номер телефона получен.")
+            Phone = True
+        if callback.data == 'email':
+            Email = True
+        if callback.data == 'address':
+            Address = True
+
+        bot.answer_callback_query(callback.id, text="Контакты были внесены в визитку.")
+        ContactsAdd = True
 
 
 bot.polling(none_stop = True)
+
--- Markup.py
+++ Markup.py
@@ -10,15 +10,29 @@ KeyScreen = types.KeyboardButton('Меню')
 KeyGo = types.KeyboardButton('Создание онлайн-визитки')
 KeyStop = types.KeyboardButton('Постой, я не готов.')
 
-KeyAddPhoto = types.InlineKeyboardButton('Фото', callback_data='photo')
-KeyAddDiscrip = types.InlineKeyboardButton('Описание', callback_data='discription')
+###KeyAddPhoto = types.InlineKeyboardButton('Фото', callback_data='photo') --- В логистической реализации
+KeyAddDiscrip = types.InlineKeyboardButton('Подпись', callback_data='discription')
 KeyAddContacts = types.InlineKeyboardButton('Контакты', callback_data='contacts')
+
+KeyContactsPhone = types.InlineKeyboardButton('Телефон', callback_data='phone')
+KeyContactsEmail = types.InlineKeyboardButton('Эл. почта', callback_data='email')
+KeyContactsAddress = types.InlineKeyboardButton('Адрес', callback_data='address')
+
+
 #-Меню старта------------------------------------------------------------------------------------------------------------------
 markupStart = types.ReplyKeyboardMarkup(resize_keyboard=True)
 markupStart.add(StartKey, KeyHelp)
+
+
 #-Меню на экране---------------------------------------------------------------------------------------------------------------
 markupScreen = types.ReplyKeyboardMarkup()
 markupScreen.add(KeyGo, KeyStop)
+
+
 #-Меню в сообщении-------------------------------------------------------------------------------------------------------------
 markupMessage = types.InlineKeyboardMarkup()
-markupMessage.add(KeyAddPhoto, KeyAddDiscrip, KeyAddContacts)
+markupMessage.add(KeyAddDiscrip, KeyAddContacts)
+
+
+markupAddContacts = types.InlineKeyboardMarkup()
+markupAddContacts.add(KeyContactsPhone, KeyContactsEmail, KeyContactsAddress)

8f26ea54abb65ce50bb46521241cd44b0f4c2dad
klimmm <89641101+kl1mmm@users.noreply.github.com>
2022-12-06 14:37:47 +0300

    Update Setting.py

--- Setting.py
+++ Setting.py
@@ -1,3 +1,3 @@
 #Файл с настройками тут могут хранится все необходимые настройки для бота
 
-ApiKeyTelegramBot = "5232358847:AAFao6u3asH4IXy90d9EkKBlr_0truRTqZM" #Ключ
+ApiKeyTelegramBot = "#" #Ключ
